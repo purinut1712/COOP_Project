dc10fd60065c5841e5ea3bb86d86ddd626eb4958
purinut1712 <purinut.krut@gmail.com>
2026-03-20 14:29:48 +0700

    update code

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-export default axios.create({
-  baseURL: 'http://20.24.17.205/api'
+const api = axios.create({
+  baseURL: 'http://20.24.17.205/api' // ห้ามมี / ปิดท้าย
 });
